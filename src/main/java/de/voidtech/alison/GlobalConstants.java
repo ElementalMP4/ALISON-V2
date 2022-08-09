a4895e95522fae2afa22391ee461c9a9a13f666f
Seb <seebeesbt@gmail.com>
2022-08-09 19:13:01 +0100

    seems about right

--- src/main/java/de/voidtech/alison/GlobalConstants.java
+++ src/main/java/de/voidtech/alison/GlobalConstants.java
@@ -1,6 +1,6 @@
 package main.java.de.voidtech.alison;
 
 public class GlobalConstants {
-	public static final String VERSION = "1.0.0 - Rewrite Ristretto";
+	public static final String VERSION = "1.3.0 - Rewrite Ristretto";
 	public static final String REPO_URL = "https://github.com/Elementalmp4/ALISON-V2";
 }
