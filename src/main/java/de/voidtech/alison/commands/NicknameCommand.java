be039165d21d3ef43c3664e58d7cc404708e74e2
Seb <seebeesbt@gmail.com>
2022-08-10 19:32:22 +0100

    Added the ability to change other pople's nicks

--- src/main/java/de/voidtech/alison/commands/NicknameCommand.java
+++ src/main/java/de/voidtech/alison/commands/NicknameCommand.java
@@ -6,25 +6,40 @@ import main.java.de.voidtech.alison.entities.AlisonModel;
 import main.java.de.voidtech.alison.utils.PackManager;
 import main.java.de.voidtech.alison.utils.Responder;
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 
 public class NicknameCommand extends AbstractCommand {
 
 	@Override
 	public void execute(Message message, List<String> args) {
-		AlisonModel model = PackManager.getPack(message.getAuthor().getId());
+		Member member;
+    	if (args.isEmpty()) member = message.getMember();
+    	else {
+    		if (!message.getMember().getPermissions().contains(Permission.NICKNAME_MANAGE)) {
+    			Responder.sendAsReply(message, "You don't have permission to change other people's nicknames!");
+				return;
+    		}
+    		member = message.getGuild().getMemberById(args.get(0).replaceAll("([^0-9])", ""));
+    		if (member == null) {
+    			Responder.sendAsReply(message, "I couldn't find that user!");
+				return;
+    		}
+    	}
+		
+		AlisonModel model = PackManager.getPack(member.getId());
 		String nickname = model.createNickname();
-		if (nickname == null) Responder.sendAsReply(message, "I don't have enough information to make a nickname for you!");
+		if (nickname == null) Responder.sendAsReply(message, "I don't have enough information to make a nickname!	");
 		else {
 			if (!message.getGuild().getSelfMember().hasPermission(Permission.NICKNAME_MANAGE)) {
 				Responder.sendAsReply(message, "I don't have permission to change your nickname! Please make sure I have the `Manage Nicknames` permission!");
 				return;
 			}
-			if (!message.getGuild().getSelfMember().canInteract(message.getMember())) {
+			if (!message.getGuild().getSelfMember().canInteract(member)) {
 				Responder.sendAsReply(message, "I don't have permission to change your nickname! I need my role to be above your highest role!");
 				return;
 			}
-			message.getMember().modifyNickname(nickname).complete();
+			member.modifyNickname(nickname).complete();
 			Responder.sendAsReply(message, "Nickname changed to **" + nickname + "**");
 		}
 	}
