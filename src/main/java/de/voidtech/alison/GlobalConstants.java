38d2422905ff05a3be5595c48593b1092559fd2b
Seb <seebeesbt@gmail.com>
2022-08-16 21:46:56 +0100

    INCREMENT THE VERSION STOP FORGETTING

--- src/main/java/de/voidtech/alison/GlobalConstants.java
+++ src/main/java/de/voidtech/alison/GlobalConstants.java
@@ -1,6 +1,6 @@
 package main.java.de.voidtech.alison;
 
 public class GlobalConstants {
-	public static final String VERSION = "1.4.0 - Capybara Chaos";
+	public static final String VERSION = "1.4.1 - Capybara Chaos";
 	public static final String REPO_URL = "https://github.com/Elementalmp4/ALISON-V2";
 }
